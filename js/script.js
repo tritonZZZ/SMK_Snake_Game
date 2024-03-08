4c49d5ce327da467deda32f08e21de40ac166de0
Triton <73620841+tritonZZZ@users.noreply.github.com>
2024-03-08 10:34:25 +0200

    snake array, unshift, and pop

--- js/script.js
+++ js/script.js
@@ -13,12 +13,34 @@ let food = {
     y: Math.floor(Math.random()*15 + 3)*box
 }
 
+let snake = [];
+snake[0] = {
+    x: 9*box,
+    y: 10*box
+}
+
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
     ctx.drawImage(foodImg, food.x, food.y);
 
+    for(let i = 0; i < snake.length; i++){
     ctx.fillStyle = "green";
-    ctx.fillRect(snake[0].x, snake[0].y, box, box);
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snake.pop();
+
+    snakeX += box;
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
 let game = setInterval(drawGame, 10);
