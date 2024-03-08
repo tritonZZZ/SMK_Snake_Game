6d28346e2285cb2166f0d755a974eaf21442e3c2
PatriksM <pmorevs@gmail.com>
2024-03-08 10:13:58 +0200

    Random food added and adjusted

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@ const ctx = cnvs.getContext('2d');
 const ground = new Image();
 ground.src = "img/ground.png";
 
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+let box = 32;
+let food = {
+    x: Math.floor(Math.random()*17 + 1)*box,
+    y: Math.floor(Math.random()*15 + 3)*box
+}
+
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
+    ctx.drawImage(foodImg, food.x, food.y);
 }
 
 let game = setInterval(drawGame, 10);
